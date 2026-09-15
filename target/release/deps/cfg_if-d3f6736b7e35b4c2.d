ca99d9d48523b7412612342b51709c7809a81d15
idunnowutuwant <idunnowutuwant@gmail.com>
2026-09-15 22:15:23 +0900

    chore: remove target build cache and add .gitignore

--- target/release/deps/cfg_if-d3f6736b7e35b4c2.d
+++
@@ -1,7 +0,0 @@
-C:\Users\idnwuw\Desktop\wirecut\target\release\deps\cfg_if-d3f6736b7e35b4c2.d: C:\Users\idnwuw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
-
-C:\Users\idnwuw\Desktop\wirecut\target\release\deps\libcfg_if-d3f6736b7e35b4c2.rlib: C:\Users\idnwuw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
-
-C:\Users\idnwuw\Desktop\wirecut\target\release\deps\libcfg_if-d3f6736b7e35b4c2.rmeta: C:\Users\idnwuw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
-
-C:\Users\idnwuw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs:
