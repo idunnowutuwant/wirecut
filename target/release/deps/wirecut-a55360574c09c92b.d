6cc4742bfebc508b8e3fd0f51a88a484e7220a77
idunnowutuwant <idunnowutuwant@gmail.com>
2026-09-16 19:52:50 +0900

    chore: purge target build cache from git tracking and remove legacy artifacts

--- target/release/deps/wirecut-a55360574c09c92b.d
+++
@@ -1,5 +0,0 @@
-C:\Users\idnwuw\Desktop\wirecut\target\release\deps\wirecut-a55360574c09c92b.d: src\main.rs
-
-C:\Users\idnwuw\Desktop\wirecut\target\release\deps\wirecut-a55360574c09c92b.exe: src\main.rs
-
-src\main.rs:
--- target/release/deps/wirecut.d
+++
@@ -1,5 +0,0 @@
-C:\Users\idnwuw\Desktop\wirecut\target\release\deps\wirecut.d: src\main.rs
-
-C:\Users\idnwuw\Desktop\wirecut\target\release\deps\wirecut.exe: src\main.rs
-
-src\main.rs:
